2c4542ca38a500ae520846aab28d34db20eb1fb0
JARNEEX <ogunfuyejeh@gmail.com>
2023-01-21 07:50:54 -0800

    task

---
+++ _putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - print character c
+ * @c: character printed
+ * 
+ * Return: Always 0
+ */
+
+int _putchar(char c)
+{
+	putchar(c);
+	return(0);
+}
